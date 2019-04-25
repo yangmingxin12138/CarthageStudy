c56a062e39578e205b6cc25254b64e71f56a6db7
杨明鑫 <yangmingxin@tuhu.cn>
2019-04-25 11:08:41 +0800

    创建carthage资源

---
+++ CarthageStudy/CarthageStudy.h
@@ -0,0 +1,19 @@
+//
+//  CarthageStudy.h
+//  CarthageStudy
+//
+//  Created by ymx on 2019/4/25.
+//  Copyright © 2019 ymx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageStudy.
+FOUNDATION_EXPORT double CarthageStudyVersionNumber;
+
+//! Project version string for CarthageStudy.
+FOUNDATION_EXPORT const unsigned char CarthageStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageStudy/PublicHeader.h>
+
+
